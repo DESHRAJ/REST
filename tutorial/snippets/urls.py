e94fbd2cf5dbe9b6f58cf2f41e5a019f1848d72f
Deshraj <deshrajdry@gmail.com>
2015-05-30 06:49:55 +0530

    using the generic classes for different requests

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import url
 from snippets import views
 
+'''the below module import is used for importing the foramt type in which we 
+the data like json or xml'''
+from rest_framework.urlpatterns import format_suffix_patterns
+
+
 urlpatterns = [
-	url(r'^snippets/$', views.snippet_list),
-	url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
+	url(r'^snippets/$', views.SnippetList.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
